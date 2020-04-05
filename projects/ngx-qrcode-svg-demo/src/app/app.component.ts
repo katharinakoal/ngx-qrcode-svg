1368437921bfceb391452fa2f67ddaa59e0a3c8b
kkoal <k.koal@student.htw-berlin.de>
2020-04-05 21:34:51 +0200

    chore: prepare demonstrator

--- projects/ngx-qrcode-svg-demo/src/app/app.component.ts
+++ projects/ngx-qrcode-svg-demo/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ngx-qrcode-svg-demo';
+  qrData: string;
+  errorCorrectionLevel: string;
 }
--- projects/ngx-qrcode-svg-demo/src/app/app.module.ts
+++ projects/ngx-qrcode-svg-demo/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { QRCodeSVGModule } from 'ngx-qrcode-svg';
 
@@ -6,7 +7,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, QRCodeSVGModule],
+  imports: [BrowserModule, FormsModule, QRCodeSVGModule],
   providers: [],
   bootstrap: [AppComponent],
 })
