f62e6390de2215acfd515f79c67ad67243ae4c8a
kkoal <k.koal@student.htw-berlin.de>
2020-04-06 22:40:43 +0200

    feat(lib): create qrcode with node-qrcode

--- projects/ngx-qrcode-svg-demo/src/app/app.component.ts
+++ projects/ngx-qrcode-svg-demo/src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-qrcode-svg-demo';
-  qrData: string;
+  qrData = 'test';
   errorCorrectionLevel: string;
 }
