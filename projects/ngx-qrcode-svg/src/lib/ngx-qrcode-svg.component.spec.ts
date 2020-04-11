d4608572c0b87ff6712ebbf47d42b17ecc473076
kkoal <k.koal@student.htw-berlin.de>
2020-04-11 14:47:38 +0200

    test: prepare tests

--- projects/ngx-qrcode-svg/src/lib/ngx-qrcode-svg.component.spec.ts
+++ projects/ngx-qrcode-svg/src/lib/ngx-qrcode-svg.component.spec.ts
@@ -1,24 +1,62 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component, ViewChild } from '@angular/core';
 import { QRCodeSVGComponent } from './ngx-qrcode-svg.component';
+import { ErrorCorrectionLevel } from './ngx-qrcode-svg.types';
+@Component({
+  template: ` <qrcode-svg
+    [value]="qrCodeValue"
+    [errorCorrectionLevel]="errorCorrectionLevel"
+    [margin]="margin"
+    [color]="color"
+    [backgroundColor]="backgroundColor"
+  ></qrcode-svg>`,
+})
+class TestHostComponent {
+  @ViewChild(QRCodeSVGComponent, { static: true })
+  public qrCodeSVGComponent: QRCodeSVGComponent;
+  qrCodeValue: string;
+  errorCorrectionLevel: ErrorCorrectionLevel;
+  margin: number;
+  color: string;
+  backgroundColor: string;
+}
 
-describe('NgxQrcodeSvgComponent', () => {
+describe('QRCodeSVGComponent', () => {
   let component: QRCodeSVGComponent;
-  let fixture: ComponentFixture<QRCodeSVGComponent>;
+  let hostComponent: TestHostComponent;
+  let hostFixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [QRCodeSVGComponent],
+      declarations: [QRCodeSVGComponent, TestHostComponent],
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(QRCodeSVGComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    hostFixture = TestBed.createComponent(TestHostComponent);
+    hostComponent = hostFixture.componentInstance;
+    component = hostComponent.qrCodeSVGComponent;
+    hostFixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+
+    hostComponent.qrCodeValue = 'test';
+    hostComponent.errorCorrectionLevel = 'H';
+    hostComponent.margin = 0;
+    hostComponent.color = '#000';
+    hostComponent.backgroundColor = '#fff';
+    hostFixture.detectChanges();
+
+    console.log(component);
+
+    // component.value = 'yo!';
+    // jasmine.createSpy();
+    // component.ngOnChanges();
+    // delete component.value;
+    // component.ngOnChanges();
+    // fixture.detectChanges();
+    // console.log(fixture.debugElement);
   });
 });
