5d35852bce0d88caa93e4ba3262010e53e71db0d
kkoal <k.koal@student.htw-berlin.de>
2020-04-15 09:46:12 +0200

    chore: improve ci setup

--- projects/ngx-qrcode-svg-demo/karma.conf.js
+++ projects/ngx-qrcode-svg-demo/karma.conf.js
@@ -25,14 +25,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: [process.env.CI ? 'ChromeHeadlessCI' : 'Chrome'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox'],
       },
     },
-    singleRun: false,
+    singleRun: process.env.CI,
     restartOnFileChange: true,
   });
 };
